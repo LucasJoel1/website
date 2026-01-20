51d1a3b0498c3fec9c0e9681fc3e170e12366426
LucasJoel1 <ljoel1@outlook.com>
2026-01-20 02:21:46 -0500

    fixed base url

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-  base: "https://lucasjoel.com"
+  base: "https://lucasjoel.com/"
 });
