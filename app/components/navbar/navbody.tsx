ce6c946afe349ae2aa035dccf7aab7f89bdbfa97
LucasJoel1 <ljoel1@outlook.com>
2026-01-29 00:16:59 -0500

    chore: scaffold CSS framework (issue #1)

--- app/components/navbar/navbody.tsx
+++ app/components/navbar/navbody.tsx
@@ -9,12 +9,46 @@ export default function NavBody(props: NavBodyProps) {
 
     return (
         <div className="p-4 flex flex-col gap-1">
-            <Button stretch={true} selected={true} href="/" onClick={handleClick}>Home</Button>
-            <Button stretch={true} selected={false} href="/about" onClick={handleClick}>About</Button>
-            <Button stretch={true} selected={false} href="/resume" onClick={handleClick}>Resume</Button>
-            <Button stretch={true} selected={false} href="/projects" onClick={handleClick}>Projects</Button>
-            <Button stretch={true} selected={false} href="/blog" onClick={handleClick}>Blog</Button>
-            <Button stretch={true} selected={false} href="/contact" onClick={handleClick}>Contact</Button>
+            <Button
+                stretch={true}
+                selected={true}
+                href="/"
+                onClick={handleClick}
+            >
+                Home
+            </Button>
+            <Button
+                stretch={true}
+                selected={false}
+                href="/about"
+                onClick={handleClick}
+            >
+                About
+            </Button>
+            <Button
+                stretch={true}
+                selected={false}
+                href="/projects"
+                onClick={handleClick}
+            >
+                Projects
+            </Button>
+            <Button
+                stretch={true}
+                selected={false}
+                href="/blog"
+                onClick={handleClick}
+            >
+                Blog
+            </Button>
+            <Button
+                stretch={true}
+                selected={false}
+                href="/contact"
+                onClick={handleClick}
+            >
+                Contact
+            </Button>
         </div>
-    )
+    );
 }
